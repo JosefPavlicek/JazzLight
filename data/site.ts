7f4202b3290a2590b315c51fe4903c46cdc5a703
JosefPavlicek <josef.pavlicek@gmail.com>
2026-05-26 09:07:45 +0000

    Uprava site.ts

--- data/site.ts
+++ data/site.ts
@@ -1,9 +1,11 @@
 import type { Concert } from "@/types/concert";
 
+
 export const aboutText =
   "JazzLight je komorní hudební projekt zaměřený na příjemnou, elegantní a posluchačsky vstřícnou hudbu. Repertoár sahá od jazzu přes popové melodie až po známé filmové a muzikálové skladby.";
 
-export const repertoireHtml = `
+
+  export const repertoireHtml = `
   <p>V repertoáru najdete známé melodie z oblasti jazzu, popu, filmové hudby i komorních aranží pro kulturní a společenské události.</p>
   <ul>
     <li>jazzové standardy</li>
@@ -12,10 +14,12 @@ export const repertoireHtml = `
   </ul>
 `;
 
+
 export const members = [
   { id: "josef", name: "Josef Pavlíček", role: "kytara, aranž", image: "/img/josef.svg" },
   { id: "singer", name: "Zpěvačka", role: "zpěv", image: "/img/singer.svg" }
 ];
+
 
 export const mockConcerts: Concert[] = [
   {
